4eea41e36ecb6aee8b3fc700657593c6776c1d14
yl5619 <yl5619@ic.ac.uk>
2025-03-24 14:39:38 +0000

    update build script

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-README under construction...
+### Environment Setup
+
+CAST is built with cmake and depends on the [LLVM project](https://github.com/llvm/llvm-project).
+
+CAST is under active developments. The development enviromnent of CAST requires a specific structure of LLVM installation, and we recommand compiling and installing the LLVM project afresh.
+
+
--- build_llvm.sh
+++ build_llvm.sh
@@ -1,52 +1,30 @@
 #! /bin/shell
 
+cast_llvm_root = ($1)
+llvm_version = ($2)
 
-# Download the installer and run the following commands in your terminal
-# bash Miniconda3-latest-Linux-x86_64.sh  # For Linux
-
-# # Initialize conda
-# conda init
-
-# # Close and reopen the terminal, then create and activate your environment
-# conda create -n myenv
-# conda activate myenv
-
-# # Install the compilers package
-# conda install -c conda-forge compilers
-# # unicode/ucnv.h file
-# conda install icu
-
-## To remove all cuda packages in conda enviroment
-## After activate the enviroment, run
-# conda remove $(conda list | grep cuda | awk '{print $1}')
-## To also clean cache, run
-# conda clean --all
-
-export llvm_root=`pwd`
-export llvm_version=19.1.0
-
-cmake -S llvm-project-${llvm_version}.src/llvm -G Ninja \
+cmake -S ${cast_llvm_root}/llvm-project-${llvm_version}.src/llvm -G Ninja \
 -B release-build \
 -DCMAKE_BUILD_TYPE=Release \
 -DLLVM_ENABLE_RTTI=ON \
 -DLLVM_TARGETS_TO_BUILD="Native;NVPTX" \
 -DLLVM_ENABLE_PROJECTS="clang;lld;lldb" \
--DLLVM_ENABLE_RUNTIMES="libcxx;libcxxabi;libunwind"
+-DLLVM_ENABLE_RUNTIMES="libcxx;libcxxabi;libunwind;compiler-rt"
 
 cmake --build release-build
 
-cmake --install release-build --prefix "release-install"
+cmake --install release-build --prefix "${cast_llvm_root}/release-install"
 
-cmake -S llvm-project-${llvm_version}.src/llvm -G Ninja \
+cmake -S ${cast_llvm_root}/llvm-project-${llvm_version}.src/llvm -G Ninja \
 -B debug-build \
 -DCMAKE_BUILD_TYPE=Debug \
 -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON \
 -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=ON \
 -DLLVM_ENABLE_RTTI=ON \
 -DLLVM_TARGETS_TO_BUILD="Native;NVPTX" \
--DCMAKE_C_COMPILER="$llvm_root/release-install/bin/clang" \
--DCMAKE_CXX_COMPILER="$llvm_root/release-install/bin/clang++" \
--DLLVM_USE_LINKER="$llvm_root/release-install/bin/ld.lld"
+-DCMAKE_C_COMPILER="${cast_llvm_root}/release-install/bin/clang" \
+-DCMAKE_CXX_COMPILER="${cast_llvm_root}/release-install/bin/clang++" \
+-DLLVM_USE_LINKER="${cast_llvm_root}/release-install/bin/ld.lld"
 
 cmake --build debug-build
 
